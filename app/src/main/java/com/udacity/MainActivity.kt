bfadfdb03db4b4c14b3f077df413637aef0c4c36
DA <durim.aliu5@gmail.com>
2021-12-24 13:37:15 +0100

    Impl main screen.

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,61 +1,180 @@
 package com.udacity
 
 import android.app.DownloadManager
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.ContentObserver
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import kotlinx.android.synthetic.main.content_main.*
+import androidx.core.net.toUri
+import androidx.databinding.DataBindingUtil
+import com.udacity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var downloadID: Long = 0
+    private lateinit var viewBinding: ActivityMainBinding
 
-    private lateinit var notificationManager: NotificationManager
-    private lateinit var pendingIntent: PendingIntent
-    private lateinit var action: NotificationCompat.Action
+    private var downloadFileName = ""
+    private var downloadID: Long = NO_DOWNLOAD
+    private var downloadContentObserver: ContentObserver? = null
+    private var downloadNotificator: DownloadNotificator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                viewBinding = this
+                setSupportActionBar(toolbar)
+                registerReceiver(
+                    onDownloadCompletedReceiver,
+                    IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+                )
+                onLoadingButtonClicked()
+            }
+    }
 
-        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
-        custom_button.setOnClickListener {
-//            download()
+    private fun ActivityMainBinding.onLoadingButtonClicked() {
+        with(mainContent) {
+            loadingButton.setOnClickListener {
+                when (downloadOptionRadioGroup.checkedRadioButtonId) {
+                    View.NO_ID ->
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Please select the file to download",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    else -> {
+                        downloadFileName =
+                            findViewById<RadioButton>(downloadOptionRadioGroup.checkedRadioButtonId)
+                                .text.toString()
+                        requestDownload()
+                    }
+                }
+            }
         }
     }
 
-    private val receiver = object : BroadcastReceiver() {
+    private val onDownloadCompletedReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            id?.let {
+                val downloadStatus = getDownloadManager().queryStatus(it)
+                unregisterDownloadContentObserver()
+                downloadStatus.takeIf { status -> status != DownloadStatus.UNKNOWN }?.run {
+                    getDownloadNotificator().notify(downloadFileName, downloadStatus)
+                }
+            }
         }
     }
 
-    private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
+    private fun getDownloadNotificator(): DownloadNotificator = when (downloadNotificator) {
+        null -> DownloadNotificator(this, lifecycle).also { downloadNotificator = it }
+        else -> downloadNotificator!!
+    }
+
+    private fun DownloadManager.queryStatus(id: Long): DownloadStatus {
+        query(DownloadManager.Query().setFilterById(id)).use {
+            with(it) {
+                if (this != null && moveToFirst()) {
+                    return when (getInt(getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+                        DownloadManager.STATUS_SUCCESSFUL -> DownloadStatus.SUCCESSFUL
+                        DownloadManager.STATUS_FAILED -> DownloadStatus.FAILED
+                        else -> DownloadStatus.UNKNOWN
+                    }
+                }
+                return DownloadStatus.UNKNOWN
+            }
+        }
+    }
+
+    private fun requestDownload() {
+        with(getDownloadManager()) {
+            downloadID.takeIf { it != NO_DOWNLOAD }?.run {
+                val downloadsCancelled = remove(downloadID)
+                unregisterDownloadContentObserver()
+                downloadID = NO_DOWNLOAD
+            }
+
+            val request = DownloadManager.Request(Uri.parse(URL))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            downloadID = enqueue(request)
+
+            createAndRegisterDownloadContentObserver()
+        }
+    }
+
+    private fun DownloadManager.createAndRegisterDownloadContentObserver() {
+        object : ContentObserver(Handler(Looper.getMainLooper())) {
+            override fun onChange(selfChange: Boolean) {
+                downloadContentObserver?.run { queryProgress() }
+            }
+        }.also {
+            downloadContentObserver = it
+            contentResolver.registerContentObserver(
+                "content://downloads/my_downloads".toUri(),
+                true,
+                downloadContentObserver!!
+            )
+        }
+    }
+
+    private fun DownloadManager.queryProgress() {
+        query(DownloadManager.Query().setFilterById(downloadID)).use {
+            with(it) {
+                if (this != null && moveToFirst()) {
+                    val id = getInt(getColumnIndex(DownloadManager.COLUMN_ID))
+                    when (getInt(getColumnIndex(DownloadManager.COLUMN_STATUS))) {
+                        DownloadManager.STATUS_FAILED -> {
+                            viewBinding.mainContent.loadingButton.changeButtonState(ButtonState.Completed)
+                        }
+                        DownloadManager.STATUS_PAUSED -> {
+
+                        }
+                        DownloadManager.STATUS_PENDING -> {
+
+                        }
+                        DownloadManager.STATUS_RUNNING -> {
+                            viewBinding.mainContent.loadingButton.changeButtonState(ButtonState.Loading)
+                        }
+                        DownloadManager.STATUS_SUCCESSFUL -> {
+                            viewBinding.mainContent.loadingButton.changeButtonState(ButtonState.Completed)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(onDownloadCompletedReceiver)
+        unregisterDownloadContentObserver()
+        downloadNotificator = null
+    }
+
+    private fun unregisterDownloadContentObserver() {
+        downloadContentObserver?.let {
+            contentResolver.unregisterContentObserver(it)
+            downloadContentObserver = null
+        }
     }
 
     companion object {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+        private const val NO_DOWNLOAD = 0L
     }
-
 }
